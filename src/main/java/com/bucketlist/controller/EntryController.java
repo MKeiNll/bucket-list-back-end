881b93b975b3012510787a767cf96bb90a3dbf7b
MKeiNll <levgirich@gmail.com>
2019-03-05 19:08:00 +0200

    Initial commit

--- src/main/java/com/bucketlist/controller/EntryController.java
+++ src/main/java/com/bucketlist/controller/EntryController.java
@@ -1,11 +1,20 @@
 package com.bucketlist.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.bucketlist.model.Entry;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EntryController {
 
-    public String getHello() {
-        return "hello";
+    /**
+     * GET /realestate : Lists all real estate objects.
+     *
+     * @return body with all real estate objects.
+     */
+    @CrossOrigin(origins = "http://localhost:9000")
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public Entry getTestEntry() {
+        return new Entry("hello, ", "react");
     }
 }
